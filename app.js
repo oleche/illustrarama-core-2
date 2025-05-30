13da98bcd0e47be7afad544ac281cd53514d7e84
Oscar Fernando Leche Bustamante <oscar.leche@gmail.com>
2025-05-31 01:30:11 +0200

    removing cors

--- app.js
+++ app.js
@@ -79,13 +79,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
-app.use(cors());
+// app.use(cors());
 
 app.use('/', index);
 app.use('/api/v1/news', news);
