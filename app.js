5b68ca0a7b1e15b1db86cfbe672b72b19486743d
Oscar Fernando Leche Bustamante <oscar.leche@gmail.com>
2025-05-31 01:36:05 +0200

    extends cors

--- app.js
+++ app.js
@@ -79,13 +79,17 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
-// app.use(cors());
+app.use(cors());
 
 app.use('/', index);
 app.use('/api/v1/news', news);
